d2a3c56f679c574dbde28c6dde4f681c42f9b4d9
Nguyen Minh Tho <minhthoj27@icloud.com>
2024-06-05 16:46:31 +0700

    Setup Million

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,20 @@
+import MillionLint from '@million/lint';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				hostname: 'api.dicebear.com',
-			},
-			{
-				hostname: 'jzqegimyakethcnqrtaf.supabase.co', // development
-			},
-			{
-				hostname: 'files.edgestore.dev', // production
-			},
-			{
-				hostname: 'cdn.toby7002.dev',
-			},
-			{
-				hostname: 'dummyjson.com',
-			},
-		],
-	},
-}
-
-export default nextConfig
+  images: {
+    remotePatterns: [{
+      hostname: 'api.dicebear.com'
+    }, {
+      hostname: 'jzqegimyakethcnqrtaf.supabase.co' // development
+    }, {
+      hostname: 'files.edgestore.dev' // production
+    }, {
+      hostname: 'cdn.toby7002.dev'
+    }, {
+      hostname: 'dummyjson.com'
+    }]
+  }
+};
+export default MillionLint.next({
+  rsc: true
+})(nextConfig);
